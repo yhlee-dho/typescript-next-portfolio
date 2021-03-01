994f984963b52c1d223711f7036e15c7538adc3f
Yong Lee <yhlee89.dho@gmail.com>
2021-02-28 18:45:35 -0600

    init with tailwind config

---
+++ global.d.ts
@@ -0,0 +1,21 @@
+declare module 'fs';
+declare module 'tailwindcss-dark-mode';
+declare module 'to-vfile';
+declare module 'remark-images';
+import DomUtils from 'node_modules/domutils/index';
+declare const DomUtils;
+
+import {
+	IconDefinition,
+	IconPrefix,
+	IconName
+} from '@fortawesome/fontawesome-common-types';
+export const definition: IconDefinition;
+export const faWindowClose: IconDefinition;
+export const prefix: IconPrefix;
+export const iconName: IconName;
+export const width: number;
+export const height: number;
+export const ligatures: string[];
+export const unicode: string;
+export const svgPathData: string;
---
+++ site.config.ts
@@ -0,0 +1,15 @@
+export default {
+  siteMetadata: {
+    title: "typescript-next-portfolio",
+    author: {
+      name: "Yong Lee",
+      summary: "Typescript Next portfolio styled with Tailwind.css",
+    },
+    description: "Portfolio created with Next.js and Tailwind.css",
+    siteUrl: "https://typescript-next-portfolio-yhlee.vercel.app/",
+    language: "en-US",
+    social: {
+      twitter: "YongLee",
+    },
+  },
+};
