903972cf7e0ac857d2beff3435d9f2841cc649f2
Yong Lee <yhlee89.dho@gmail.com>
2021-03-05 00:35:17 -0600

    added about me image

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,11 +17,13 @@ import { GetStaticProps } from 'next';
 import AboutType from 'types/about';
 import Alert from '../components/alert';
 
+import cn from 'classnames';
 
 interface AboutProps {
+	about: AboutType;
 	props: string | number;
 	preview?: boolean;
-	about: AboutType;
+	slug?: string;
 }
 
 
@@ -37,45 +39,63 @@ const About = ({ about, props, preview }: AboutProps) => {
 
 					
 				</Head>
+				{/* <Container>
+					<div
+
+					>
+
+
+					</div>
+				</Container> */}
 				<Container>
 					<br />
-					
-					<h2 className='hover:text-tailwindBlue text-black text-3xl md:text-3xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+					<img
+						src={'https://res.cloudinary.com/yongstal/image/upload/v1614658682/portfolio/SAM_3421_kmvghe.jpg'}
+						alt={`${CLIENT_NAME}'s cover image`}
+						className={'w-50vw'}
+					/>
+
+					<h2 className='hover:text-tailwindBlue text-black text-6xl md:text-3xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
 						<Link href='/about'>
-							<a className='hover:text-tailwindBlue text-black px-8'>{ `Hello, my name is ${CLIENT_NAME}!`}</a>
+							<a className='hover:text-tailwindBlue text-black'>{ `Hello, my name is ${CLIENT_NAME}!`}</a>
 						</Link>
 						{/* {`Hello, my name is ${CLIENT_NAME}!`} */}
 						
 					</h2>
-					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-3x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
 						I am a full stack web developer at Windy City Devs.
 						</p>
 					
-					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-3x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
 						On July of 2020 I co-founded Windy City Devs with Joe Shields and Andrew Ross.
 						Our goal was to immerse ourselves into the web development industry so that we may improve our coding skills
 						and we strive to go over and beyond each day.
 						</p>
-					
-					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+			
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-3x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
 						At Windy City Devs, we utilize Typescript for its superior error handling capabilities,
 						and Next.js as our framework of choice for its compatability with Typescript and its amazingly simple Vercel intedration for deployment.
-						Our styling of choice is Tailwinds.css for its ease of utilizing class names.
+						
+						
+						</p>
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-3x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+						Our styling of choice is Tailwinds.css for its ease of utilizing class names and viewport.
 						For our databases we favor MongoDB because of its accessibility to other coding languages.
-						Lastly, for our servers we utilize AWS Lightsail.
-						Althought it does lack some answers in stack overflow, its test environment allows for quick testing.
-						</p>
-					
-					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
-						I received my initial training at Northwestern University through the school of professional studies’
+						Lastly, for our servers we utilize AWS Lightsail.							
+						Althought it does lack some answers in stack overflow, its test environment allows for quick testing and eventually there is no better space than the AWS cloud to store client apps.
+					</p>
+
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-3x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+					I received my initial training at Northwestern University through the school of professional studies’
 						full stack web development program.
-						</p>
-				
-					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+					</p>
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-3x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
 						Prior to my assimilation to code, my background was in marketing and publishing.
 						Korea Times is a ethnic local newspaper company that serve 23 midwest states as a daily newspaper publisher.
 						At Korea Times I managed daily operations and edited the U.S. sections.
-						</p>
+					</p>
+
+
 					
 					{/* <PostHeader
 						title={about.title}
