a3ed1f6bc78c49a3db3d57f1531062b6e63e1aab
Yong Lee <yhlee89.dho@gmail.com>
2021-03-04 23:37:51 -0600

    edited about me text

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,6 +15,7 @@ import { getAboutBySlug } from 'lib/about-api';
 import { GetStaticProps } from 'next';
 
 import AboutType from 'types/about';
+import Alert from '../components/alert';
 
 
 interface AboutProps {
@@ -37,19 +38,44 @@ const About = ({ about, props, preview }: AboutProps) => {
 					
 				</Head>
 				<Container>
-					<br/>
-					<h2 className='hover:text-tailwindBlue text-cimaRed text-3xl md:text-3xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+					<br />
+					
+					<h2 className='hover:text-tailwindBlue text-black text-3xl md:text-3xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
 						<Link href='/about'>
-							<a className='hover:text-tailwindBlue text-cimaRed px-8'>{ `Hello!, my name is ${CLIENT_NAME}.`}</a>
+							<a className='hover:text-tailwindBlue text-black px-8'>{ `Hello, my name is ${CLIENT_NAME}!`}</a>
 						</Link>
 						{/* {`Hello, my name is ${CLIENT_NAME}!`} */}
 						
 					</h2>
-					<p>I am a full stack web developer with a background in marketing and publishing.</p>
-					<p>In order to make the transition from my previous background to the coding world I co-founded a web development company named Windy City Devs.</p>					
-					<p>Although I do constantly learn new ways to develop different websites, I received my initial training at Northwestern University through the school of professional studies’ full stack web development program.</p>
-					<p>Prior to that I was involved in a variety of different opportunities. I owned an internet cafe business, I worked for a local politician, and a local newspaper publisher.</p>					
-					<p>By utilizing all of my past experiences, I am excited to work together with coworkers to overcome challenges.</p>
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+						I am a full stack web developer at Windy City Devs.
+						</p>
+					
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+						On July of 2020 I co-founded Windy City Devs with Joe Shields and Andrew Ross.
+						Our goal was to immerse ourselves into the web development industry so that we may improve our coding skills
+						and we strive to go over and beyond each day.
+						</p>
+					
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+						At Windy City Devs, we utilize Typescript for its superior error handling capabilities,
+						and Next.js as our framework of choice for its compatability with Typescript and its amazingly simple Vercel intedration for deployment.
+						Our styling of choice is Tailwinds.css for its ease of utilizing class names.
+						For our databases we favor MongoDB because of its accessibility to other coding languages.
+						Lastly, for our servers we utilize AWS Lightsail.
+						Althought it does lack some answers in stack overflow, its test environment allows for quick testing.
+						</p>
+					
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+						I received my initial training at Northwestern University through the school of professional studies’
+						full stack web development program.
+						</p>
+				
+					<p className='hover:text-tailwindBlue text-fiveOBlack text-1x1 font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8'>
+						Prior to my assimilation to code, my background was in marketing and publishing.
+						Korea Times is a ethnic local newspaper company that serve 23 midwest states as a daily newspaper publisher.
+						At Korea Times I managed daily operations and edited the U.S. sections.
+						</p>
 					
 					{/* <PostHeader
 						title={about.title}
